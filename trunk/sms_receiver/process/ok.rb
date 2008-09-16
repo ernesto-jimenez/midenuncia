9f872fa6afcaf77df6f3a085762720dd75632093
erjica <erjica@20504554-8339-11dd-ba2e-09f2ff3e973f>
2008-09-16 22:38:17 +0000

    updates

--- trunk/sms_receiver/process/ok.rb
+++ trunk/sms_receiver/process/ok.rb
@@ -6,12 +6,21 @@ require 'environment'
 
 module Process
   class Ok
-    def self.run(user, address)
-      user = User.find_by_phone(user)
+    def self.run(phone, address)
+      user = User.find_by_phone(phone)
       if user and user.pending_video
+        puts "User #{phone} wants to confirm a video upload"
         video = user.pending_video
+        puts "Geocoding video..." unless address.strip == ""
         video.address = address
+        puts "Uploading video to YouTube..."
         video.destroy if video.upload
+        puts "Video uploaded"
+      else
+        puts "No pending videos from user #{phone}"
+        puts "Sending instructions..."
+        SmsSender.send_sms(phone, "No tienes ningun video pendiente de confirmacion")
+        puts "SMS sent"
       end
     end
 
