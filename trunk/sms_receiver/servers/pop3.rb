1ff1a2dc8af377d594afb70a9af3f012d7abcd47
erjica <erjica@20504554-8339-11dd-ba2e-09f2ff3e973f>
2008-09-18 18:59:14 +0000

    fix

--- trunk/sms_receiver/servers/pop3.rb
+++ trunk/sms_receiver/servers/pop3.rb
@@ -3,8 +3,8 @@ require 'open_movilforum'
 require 'process'
 
 # CREDENTIALS FOR POP3 SERVER
-user = ''
-pass = ''
+user = 'test.openmovilforum@gmail.com'
+pass = '12341234'
 
 server = OpenMovilforum::SMS::Receiver::POP3.new(user, pass)
 server.add_observer(Process::Proxy)
