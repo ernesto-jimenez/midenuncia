6c8df02d8a2aa46260779a83cd1917ac874101cc
ladybenko <ladybenko@20504554-8339-11dd-ba2e-09f2ff3e973f>
2008-09-18 15:30:10 +0000

    Parche con las vistas

--- trunk/rails_app/app/controllers/videos_controller.rb
+++ trunk/rails_app/app/controllers/videos_controller.rb
@@ -1,18 +1,40 @@
-class VideosController < ApplicationController
+class VideosController < ApplicationController  
+  skip_before_filter :require_login, :only => :index
+  
   # GET /videos
   # GET /videos.xml
   def index
     @videos = PendingVideo.find(:all)
-
+    
+    # gmap
+    @map = GMap.new("map")  
+    @map.control_init(:large_map => true, :map_type => true)
+    
+    marker = GMarker.new([75.6, -42.467],   
+      :title => "Where Am I?", :info_window => "Hello, Greenland")  
+    @map.overlay_init(marker)
+    
+    points = Array.new
+    i = 0
+    Video.all.each do |video|
+      if video.geotagged?
+        points.push([video.lat, video.lng])
+        @map.overlay_init(GMarker.new([video.lat, video.lng], :title => ('A'..'Z').to_a[i]))
+      end
+      i += 1
+    end
+    @map.center_zoom_on_points_init(points)
+        
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @videos }
     end
+    
   end
 
   # GET /videos/1
   # GET /videos/1.xml
-  def show
+  def show    
     @video = PendingVideo.find(params[:id])
 
     respond_to do |format|
@@ -23,7 +45,7 @@ class VideosController < ApplicationController
 
   # GET /videos/new
   # GET /videos/new.xml
-  def new
+  def new    
     @video = PendingVideo.new
 
     respond_to do |format|
